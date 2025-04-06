4a311a0f4dd297aa4018134930d7795675f9fda6
Coolz-bro <mt1288ta@gmail.com>
2025-04-06 15:36:05 +0200

    Update TicTacToe.h

--- TicTacToe.h
+++ TicTacToe.h
@@ -1,12 +1,15 @@
+#pragma once
 
 #include <iostream>
 
 using namespace std;
 
+// works like define
 enum mode
 {
-    classic = 3,
-    crazy = 10
+    CLASSIC = 3,
+    crazy = 10,
+    LENGTH=9 
 };
 
 struct player
@@ -14,30 +17,34 @@ struct player
     int number;
     string name;
     int age;
+    int wins;
 };
 
+void resetField(char* p_field)
+{
+    for (int j = 0;j < LENGTH;j++)
+    {
+        *p_field = ' ';
+        p_field++;
+    }
+}
+void welcome()
+{
+    cout << "\n\033[6;31;43m Welcome to TICTACTOE\033[0m\n\n"; //for the color 
+}
 void input_player(struct player spieler[])
 {
     for (int i = 0; i < 2; i++)
     {
-        cout << " \nYou are player (1) or (2)? ";
-        cin >> spieler[i].number;
-
-        if (spieler[i].number != 1 && spieler[i].number != 2)
-        {
-            cout << "Invalid, try again!!\n";
-            i--;
-            continue;
-        }
-
-        cout << "Enter your name: ";
+       
+        cout << "\n\nPlayer " << spieler[i].number << " :\n\nEnter your name : ";
         cin >> spieler[i].name;
         cout << "Enter your age: ";
         cin >> spieler[i].age;
     }
 }
 
-void drewField(char* p_field)
+void drawField(char* p_field)
 {
     cout << "---------------\n";
     for (int r = 0;r < 3;r++)
@@ -53,20 +60,16 @@ void drewField(char* p_field)
 
     }
 }
-bool checkWin(char field[3][3])// rules
+bool checkWin(char field[3][3])
 {
-    for (int z = 0;z < 3;z++)
+    for (int r = 0;r < 3;r++)
     {
-        for (int s = 0;s < 3;s++)
+        for (int c = 0;c < 3;c++)
         {
-            if (field[z][0] == 'X' && field[z][1] == 'X' && field[z][2] == 'X')return true;
-            else if (field[0][s] == 'X' && field[1][s] == 'X' && field[2][s] == 'X')return true;
-            else  if (field[0][0] == 'X' && field[1][1] == 'X' && field[2][2] == 'X')return true;
-            else  if (field[0][2] == 'X' && field[1][1] == 'X' && field[2][0] == 'X')return true;
-            else if (field[z][0] == 'O' && field[z][1] == 'O' && field[z][2] == 'O')return true;
-            else if (field[0][s] == 'O' && field[1][s] == 'O' && field[2][s] == 'O')return true;
-            else if (field[0][0] == 'O' && field[1][1] == 'O' && field[2][2] == 'O')return true;
-            else if (field[0][2] == 'O' && field[1][1] == 'O' && field[2][0] == 'O')return true;
+                if (field[r][0]!=' '&&field[r][0] == field[r][1] && field[r][2] == field[r][1])return true;
+                else if (field[0][c]!=' '&&field[0][c] == field[1][c] && field[2][c] == field[1][c])return true;
+                else  if (field[0][0]!=' '&&field[0][0] == field[1][1] && field[2][2] == field[1][1])return true;
+                else  if (field[0][2]!=' '&&field[0][2] == field[1][1] && field[2][0] == field[1][1])return true;
         }
     }
 
@@ -78,7 +81,7 @@ bool draw_game(char field[3][3])
     {
         for (int c = 0;c < 3;c++)
         {
-            if (field[r][c] == ' ')
+            if (field[r][c] == ' ') 
             {
                 return false;
             }
@@ -91,17 +94,19 @@ void play(enum mode Mygame, char field[3][3], struct player spieler[2])
 {
     input_player(spieler);
     system("cls"); // to clear the screen
-    int i = 0;
-  
+    int i = 0,n=0,x=0;
+    char con_input;
     int row, column;
     bool draw = false;
     for (int loop = 0; loop < Mygame; loop++)
     {
-        cout << "\n\033[6;31;43m Welcome to TICTACTOE\033[0m\n\n"; //for the color 
-        drewField(&field[0][0]);
+        welcome();
+        resetField(&field[0][0]);
+        drawField(&field[0][0]);
         while (true)
         {
-             
+        
+
             draw = draw_game(field);
             if (draw)
             {
@@ -128,58 +133,97 @@ void play(enum mode Mygame, char field[3][3], struct player spieler[2])
             if (spieler[i].number == 1)
             {
                 field[row][column] = 'X';
+               
                 if (checkWin(field))
                 {
-                    cout << endl << spieler[i].name << "  has won!!";
                     system("cls");
+                    drawField(&field[0][0]);
+                    n++;
+                    cout << "\n\n" << spieler[i].name << "  has won!!\n";
+                    spieler[i].wins = n;
+                   
                     break;
                 }
-                if (spieler[1].number == 1)
-                {
-                    i--;
-                }
-                else
-
-                    i++;
-
+                i++;
+                    system("cls");
+                    drawField(&field[0][0]);
+               
             }
             else if (spieler[i].number == 2)
             {
                 field[row][column] = 'O';
+               
                 if (checkWin(field))
                 {
-                    cout << endl << spieler[i].name << "  has won!!\n";
+                    system("cls");
+                    drawField(&field[0][0]);
+                    x++;
+                    cout << "\n\n" << spieler[i].name << "  has won!!\n";
+                    spieler[i].wins = x;
                     break;
                 }
-                if (spieler[0].number == 2)
-                {
-                    i++;
-                }
-                else
-                    i--;
+                i--;
             }
             system("cls");
-            drewField(&field[0][0]);
+            drawField(&field[0][0]);
 
         }
+        /*if (spieler[i].wins == 2 || spieler[i + 1].wins == 2)
+        {
+            if (spieler[i].wins > spieler[i + 1].wins)
+            {
+                cout << "\nPlayer 1 won " << spieler[i].wins << " matchs!!\n\n\n";
+            }
+            else
+                cout << "\nPlayer 2 won  " << spieler[i + 1].wins << " matchs!!";
+            break;
+        }*/
+        cout << "\nDo you want to continue ?,(Y)es,(N)o  ";
+        cin >> con_input;
+        if (con_input == 'N' || con_input == 'n')
+        {
+            if (spieler[0].wins == spieler[1].wins)
+            {
+                cout << "\n\nDRAW!!!";
+                break;
+            }
+
+            if (spieler[i].wins > spieler[i + 1].wins)
+            {
+                cout << "\nPlayer 1 won " << spieler[i].wins << " matchs!!\n\n\n";
+                break;
+            }
+            else
+            {
+                cout << "\nPlayer 2 won  " << spieler[i + 1].wins << " matchs!!";
+                break;
+            }
+         
+            
+        }
+        else
+            system("cls");
     }
 }
 void menu(char  field[3][3],struct player spieler[])
 {
+    system("cls");
     enum mode Mygame;
     char input=' ';
+    welcome();
     do
     {
-        cout << "Chose the mod \n"
+        system("cls");
+        cout << "\nChose the mod \n"
             << "classic , press(1) \n"
-            << "costum , press(2) \n"
-            << "crazy mode, press(3)\n"
-            << "end game, press (q) or (4) ";
+           /* << "costum , press(2) \n"*/
+            << "crazy mode, press(2)\n"
+            << "end game, press (q) or (3) ";
         cin >> input;
         switch (input)
         {
          case'1':
-            play(classic,field, spieler);
+            play(CLASSIC,field, spieler);
             break;
        case '2':
             play(crazy, field, spieler);
@@ -187,11 +231,11 @@ void menu(char  field[3][3],struct player spieler[])
        case '3':
            
        case'q':
-           cout << "END!!\n\n";
+           cout << "\n\nEND!!\n\n";
            break;
            
         default:
-            cout << "Invalid!!, please try again";
+            cout << "\nInvalid!!, please try again\n\n";
             break;
         }
     } while (input != 'q' && input != 'Q' && input != '3');
