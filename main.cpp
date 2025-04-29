98ff58cd14ee2af7d6bce53099f335c91e23a5bb
Coolz-bro <mt1288ta@gmail.com>
2025-04-29 23:17:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 
 #include<iostream>
-#include"TicTacToe.h"
+#include"spiel.h"
 
 int main()
 {
-    char field[3][3]
-    {
-        {' ',' ',' '},
-         {' ',' ',' '},
-          {' ',' ',' '},
+    char field[3][3];
+    struct player spieler[2]{
+        {1,"Mohamad",19,2},
+        {2,"Max",17,3},
     };
-
-    struct player spieler[2];
-    spieler[0].number = 1;
-    spieler[1].number = 2;
+    /*spieler[0].number = 1;
+    spieler[1].number = 2;*/
     enum mode Mygame;
-   
 
-    menu(field,spieler);
-   
+
+    menu(field, spieler);
+
 
     return 0;
 
